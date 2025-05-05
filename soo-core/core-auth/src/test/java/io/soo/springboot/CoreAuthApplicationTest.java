6fa91e27612eee69802f6293a4916e6b18b47c05
SooLee99 <dltn990930@naver.com>
2025-05-05 21:00:41 +0900

    feat: core-auth 모듈 추가
    기본적인 로그인을 구현하기 위해 core-auth 모듈을 추가하였습니다. 해당 모듈 안에 로그인을 구현할 예정입니다.

---
+++ soo-core/core-auth/src/test/java/io/soo/springboot/CoreAuthApplicationTest.java
@@ -0,0 +1,12 @@
+package io.soo.springboot;
+
+import org.junit.jupiter.api.Test;
+
+public class CoreAuthApplicationTest extends ContextTest {
+
+    @Test
+    public void shouldBeLoadedContext() {
+        // it should be passed
+    }
+
+}
